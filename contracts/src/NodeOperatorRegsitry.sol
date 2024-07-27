864dea0168e92b26892188dba7112adb4f07d54d
Saksham010 <sakshamthapa010@gmail.com>
2024-07-27 17:47:52 +0545

    Added Validator registration, operator enable/disable

---
+++ contracts/src/NodeOperatorRegsitry.sol
@@ -0,0 +1,79 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.13;
+import {INodeOperatorRegsitry} from "../interfaces/INodeOperatorRegistry.sol";
+
+
+contract NodeOperatorRegistry is INodeOperatorRegsitry{
+    uint256 operatorId;
+    mapping(address=>uint256) getOperatorIdFromAddress;
+    mapping(address=>bool) isOperatorRegistered;
+    mapping(uint256=>OperatorDetails) getOperatorDetailsFromId;
+
+    modifier checkZeroAddress(address _addr){
+        if(_addr == address(0)) revert ZeroAddress();
+        _;
+    }
+
+    function registerNodeOperator(string calldata _name, address _rewardAddress, address _controllingAddress) public checkZeroAddress(_rewardAddress) checkZeroAddress(_controllingAddress){
+
+        if(isOperatorRegistered[_controllingAddress]) revert OperatorAlreadyRegistered();
+
+        OperatorDetails memory opd = new OperatorDetails(_name,_rewardAddress,_controllingAddress,0,true);
+        getOperatorDetailsFromId[_controllingAddress] = operatorId;
+        isOperatorRegistered[_controllingAddress] = true;
+        getOperatorDetailsFromId[operatorId] = opd;
+
+        operatorId++;
+
+        emit OperatorRegistered(_controllingAddress,_rewardAddress);
+    }   
+
+    function registerValidator(bytes32[] calldata _pubkeys, bytes32[] calldata _signature) public {
+
+        if(!isOperatorRegistered[msg.sender]) revert CallerNotOperator();
+
+        ValidatorDetails memory vld = new ValidatorDetails(_pubkeys,_signature);
+        OperatorDetails memory opd = getOperatorDetailFromAddr(msg.sender);
+        opd.activeValidators++;
+        getOperatorDetailsFromId[opId] = opd;
+    }
+
+    function getOperatorDetailFromAddr(address _opAddr)public checkZeroAddress(_opAddr) returns(OperatorDetails memory) {
+        if(!isOperatorRegistered[_opAddr]) revert OperatorNotRegistered();
+
+        uint256 opid = getOperatorIdFromAddress[_opAddr];
+        OperatorDetails memory opd = getOperatorDetailsFromId[opid];
+        return opd;
+    }
+
+    function enableOperator(address _opAddr)public checkZeroAddress(_opAddr) {
+        if(!isOperatorRegistered[_opAddr]) revert OperatorNotRegistered();
+        if(msg.sender != isOperatorRegistered[_opAddr]) revert CallerNotOperator();
+
+        OperatorDetails memory opd = getOperatorDetailFromAddr(_opAddr);
+        if(opd.enabled) revert OperatorAlreadyEnabled();
+
+        opd.enabled = true;
+        uint256 opid = getOperatorIdFromAddress[_opAddr];
+        getOperatorDetailsFromId[opid] = opd;
+
+        emit OperatorEnabled(_opAddr);
+
+    }
+
+    function disableOperator(address _opAddr) public checkZeroAddress(_opAddr){
+        if(!isOperatorRegistered[_opAddr]) revert OperatorNotRegistered();
+        if(msg.sender != isOperatorRegistered[_opAddr]) revert CallerNotOperator();
+
+        OperatorDetails memory opd = getOperatorDetailFromAddr(_opAddr);
+        if(!opd.enabled) revert OperatorAlreadyDisabled();
+
+        opd.enabled = false;
+        uint256 opid = getOperatorIdFromAddress[_opAddr];
+        getOperatorDetailsFromId[opid] = opd;
+
+        emit OperatorDisabled(_opAddr);
+
+    }
+
+}
---
+++ contracts/src/interfaces/INodeOperatorRegsitry.sol
@@ -0,0 +1,122 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.13;
+interface INodeOperatorRegistry{
+    /**
+        * @dev Struct containing details to setup validator in eth staking contract
+        * @param pubkey Public keys of the validator
+        * @param signature Signature of the validator
+    */
+    struct ValidatorDetails{
+        bytes pubkey;
+        bytes signature; 
+    }
+
+    /**
+        * @dev Struct containing details of the registered node operator 
+        * @param name Node operator name
+        * @param rewardAddress Address to receive validator rewards
+        * @param controllingAddress Address of the node operator
+        * @param activeValidators Number of active validators
+        * @param enabled Operator status
+    */
+    struct OperatorDetails{
+        string name;
+        address rewardAddress; 
+        address controllingAddress; 
+        uint256 activeValidators;   
+        bool enabled;  
+    }
+
+    // ************************************
+    // ***** Events ******
+
+    /**
+        * @dev Emits when a new operator is registered
+        * @param _opAddr Address of registered operator
+        * @param _rewardAddr Reward address of the registered operator
+    */
+    event OperatorRegistered(address _opAddr,address _rewardAddr);
+
+    /**
+        * @dev Emits when an operator is enabled
+        * @param _opAddr Address of enabled operator
+    */
+    event OperatorEnabled(address _opAddr);
+
+    /**
+        * @dev Emits when an operator is disabled
+        * @param _opAddr Address of disabled operator
+    */
+    event OperatorDisabled(address _opAddr);
+    
+
+    // ************************************
+    // ***** Errors ******
+
+    /**
+        * @dev Throws when operator is already registered
+    */
+    error OperatorAlreadyRegistered();
+
+    /**
+        * @dev Throws when the caller is not an registered as operator
+    */
+    error CallerNotOperator();
+
+    /**
+        * @dev Throws when the given address is not an registered as operator
+    */
+    error OperatorNotRegistered();
+
+    /**
+        * @dev Throws when the operator is already enabled
+    */
+    error OperatorAlreadyEnabled();
+
+    /**
+        * @dev Throws when the operator is already disabled
+    */
+    error OperatorAlreadyDisabled();
+
+    /**
+        * @dev Throws when null address is encountered
+    */
+    error ZeroAddress();
+
+    // ************************************
+    // ***** External  methods ******
+
+    /**
+        * @dev Register a node operator 
+        * @param _name Node operator name
+        * @param _rewardAddress Address to receive validator rewards
+        * @param _controllingAddress Address of the node operator
+    */
+    function registerNodeOperator(string calldata _name, address _rewardAddress, address _controllingAddress) external;
+    
+    /**
+        * @dev Register a validator
+        * @param _pubkeys Public keys for the validators
+        * @param _signature Validator signature
+    */
+    function registerValidator(bytes32[] calldata _pubkeys, bytes32[] calldata _signature)external;
+
+    /**
+        * @dev Get operator detail from address
+        * @param _opAddr Operator address
+    */
+    function getOperatorDetailFromAddr(address _opAddr)external returns(OperatorDetails memory);
+
+    /**
+        * @dev Enable operator
+        * @param _opAddr Operator address
+    */
+    function enableOperator(address _opAddr)external;
+
+    /**
+        * @dev Disable operator
+        * @param _opAddr Operator address
+    */
+    function disableOperator(address _opAddr)external;
+
+}
